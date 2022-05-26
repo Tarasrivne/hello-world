300cd422790f45be508ea56e373d4c753d2e57d3
Tarasrivne <TarasViktorchuk@gmail.com>
2022-05-26 19:36:14 -0400

    First dict add to Github

---
+++ test1.py
@@ -0,0 +1,5 @@
+dict_my = {'First_name': 'Tad', 'Last_name': 'Shwed', 'age': '32', 'city': 'New York'}
+print(dict_my['First_name'])
+print(dict_my['Last_name'])
+print(dict_my['age'])
+print(dict_my['city'])
