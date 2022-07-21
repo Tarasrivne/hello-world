3ec0abfde0f5a96c96a971c170d55e94d9ceab8d
Tarasrivne <TarasViktorchuk@gmail.com>
2022-07-21 07:11:36 -0400

    Dictionary in dictionary

---
+++ work_flow/dictionary_review.py
@@ -0,0 +1,27 @@
+people = {
+    'person_1': {
+        'first_name': 'Alex',
+        'last_name': 'bure',
+        'age': '32',
+        'city': 'new york'},
+
+    'person_2': {
+        'first_name': 'don',
+        'last_name': 'digidon ',
+        'age': '23',
+        'city': 'new jersey'},
+
+    'person_3' : {
+        'first_name': 'klava',
+        'last_name': 'koka',
+        'age': '29',
+        'city': 'muhosransk'}
+}
+for username, whole_name in people.items():
+    print(f"\n Acaunt: {username}")
+    full_name = f"{whole_name['first_name']} {whole_name['last_name']}"
+    age_person = whole_name['age']
+    location = whole_name['city']
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tAge: {age_person}")
+    print(f"\tCity: {location.title()}")
