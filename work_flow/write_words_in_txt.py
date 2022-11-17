3a27bb0676afc62f9cc04043adf31f463e6361f7
Taras Viktorchuk <tarasviktorchuk@gmail.com>
2022-11-16 22:57:33 -0500

    Write some in txt file

---
+++ work_flow/write_words_in_txt.py
@@ -0,0 +1,7 @@
+try:
+    filename = 'C:\\Users\\Admin777\\Desktop\\python\\text_by_python.txt'
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} dosn't exist")
+else:
+    with open(filename, 'w') as file_object:
+        file_object.write("I leaning Python ")
