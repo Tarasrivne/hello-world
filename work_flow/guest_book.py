6e9f729300c79cbbb6d74bae820642e3a8bc02c5
Taras Viktorchuk <tarasviktorchuk@gmail.com>
2022-11-19 23:12:09 -0500

    Ask and adding name to txt file

---
+++ work_flow/guest_book.py
@@ -0,0 +1,14 @@
+filename = 'C:\\Users\\Admin777\\Desktop\\python\\text_by_python.txt'
+with open(filename, 'a') as file_object:
+
+    begin_ask = "\nWhat is your name:"
+    begin_ask += "\n(Enter 'quit' when you are finished)"
+    active = True
+    while active:
+        message = input(begin_ask)
+        if message == 'quit':
+            active = False
+        else:
+            print(f"Hello my friend {message}")
+            file_object.write(message)
+
